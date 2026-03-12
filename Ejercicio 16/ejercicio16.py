f6fc53326c8bd94e07ae602120e20bae1e906bfd
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-11 23:01:44 -0500

    Create ejercicio16.py

---
+++ Ejercicio 16/ejercicio16.py
@@ -0,0 +1,25 @@
+class DetallePedido:
+    def __init__(self, producto, cantidad):
+        self.producto = producto
+        self.cantidad = cantidad
+
+class Pedido:
+    def __init__(self, id_pedido):
+        self.id_pedido = id_pedido
+        self.detalles = []
+
+    # Composición: Se crean los detalles dentro del método
+    def agregar_producto(self, nombre, cantidad):
+        nuevo_detalle = DetallePedido(nombre, cantidad)
+        self.detalles.append(nuevo_detalle)
+
+# --- PRUEBA ---
+if __name__ == "__main__":
+    pedido_app = Pedido(901)
+    
+    pedido_app.agregar_producto("Hamburguesa Doble", 3)
+    pedido_app.agregar_producto("Papas Fritas", 2)
+
+    print(f"Pedido #: {pedido_app.id_pedido}")
+    for item in pedido_app.detalles:
+        print(f"  - {item.cantidad} unidad(es) de {item.producto}")
