73f12b07d2ecb19046687360c9dfb4e4d6bd645c
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-11 23:03:40 -0500

    Create ejercicio17.py

---
+++ Ejercicio 17/ejercicio17.py
@@ -0,0 +1,24 @@
+class Item:
+    def __init__(self, producto, cantidad):
+        self.producto = producto
+        self.cantidad = cantidad
+
+class Carrito:
+    def __init__(self):
+        self.items = []
+
+    # Composición: El carrito crea sus propios items
+    def agregar_producto(self, nombre, cantidad):
+        nuevo_item = Item(nombre, cantidad)
+        self.items.append(nuevo_item)
+
+# --- PRUEBA ---
+if __name__ == "__main__":
+    compra = Carrito()
+    
+    compra.agregar_producto("Smartphone XYZ", 1)
+    compra.agregar_producto("Funda Protectora", 1)
+
+    print("Items en tu carrito actual:")
+    for i in compra.items:
+        print(f" - {i.producto} x{i.cantidad}")
