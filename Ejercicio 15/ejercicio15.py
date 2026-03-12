fd3875fe0ffb43d657ac833ce4619ef4081715ce
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-11 22:59:40 -0500

    Create ejercicio15.py

---
+++ Ejercicio 15/ejercicio15.py
@@ -0,0 +1,25 @@
+class DetalleFactura:
+    def __init__(self, producto, precio):
+        self.producto = producto
+        self.precio = precio
+
+class Factura:
+    def __init__(self, numero):
+        self.numero = numero
+        self.detalles = []
+
+    # Composición: Se instancian los detalles dentro del método
+    def agregar_item(self, producto, precio):
+        nuevo_item = DetalleFactura(producto, precio)
+        self.detalles.append(nuevo_item)
+
+# --- PRUEBA DE FUNCIONAMIENTO ---
+if __name__ == "__main__":
+    mi_factura = Factura(505)
+    
+    mi_factura.agregar_item("Memoria RAM", 80)
+    mi_factura.agregar_item("Disco SSD", 120)
+
+    print(f"Factura ID: {mi_factura.numero}")
+    for item in mi_factura.detalles:
+        print(f" > {item.producto}: ${item.precio}")
