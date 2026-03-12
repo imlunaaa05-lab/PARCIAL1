beec8db9e8fc012cc171732af2e6626d7337da22
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-11 22:49:34 -0500

    Create ejercicio12.py

---
+++ Ejercicio 12/ejercicio12.py
@@ -0,0 +1,23 @@
+class Doctor:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+class Hospital:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.staff = []
+
+    def vincular_doctor(self, doctor):
+        self.staff.append(doctor)
+
+# Prueba
+dr1 = Doctor("Pérez")
+dr2 = Doctor("López")
+mi_hospital = Hospital("Clínica San José")
+
+mi_hospital.vincular_doctor(dr1)
+mi_hospital.vincular_doctor(dr2)
+
+print(f"Staff de {mi_hospital.nombre}:")
+for d in mi_hospital.staff:
+    print(f" - {d.nombre}")
