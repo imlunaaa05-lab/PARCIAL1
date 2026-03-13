d453a1098fde4b0c8906210ec1a256b39dcf7bd8
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-13 10:52:21 -0500

    Update ejercicio7.py

--- Ejercicio 7/ejercicio7.py
+++ Ejercicio 7/ejercicio7.py
@@ -15,21 +15,21 @@ class Estudiante:
             self.cursos.remove(curso)
             print(f"{self.nombre} se dio de baja de: {curso.titulo}")
 
-# --- PRUEBA ---
+
 if __name__ == "__main__":
-    # Instancias separadas
+  
     ia = CursoVirtual("IA para Negocios")
     ux = CursoVirtual("Diseño UX")
     user = Estudiante("Marta")
 
-    # Matricular
+    
     user.matricular(ia)
     user.matricular(ux)
 
-    # Abandonar (Asociación débil)
+    
     user.abandonar(ux)
 
-    # Corroborar
+ 
     print(f"Lista final de {user.nombre}:")
     for c in user.cursos:
         print(f"  > {c.titulo}")
