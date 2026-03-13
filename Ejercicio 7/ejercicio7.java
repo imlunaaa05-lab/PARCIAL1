998d49381d247016f8837f275445ee6acd6304f6
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-13 10:51:59 -0500

    Update ejercicio7.java

--- Ejercicio 7/ejercicio7.java
+++ Ejercicio 7/ejercicio7.java
@@ -31,19 +31,19 @@ class Estudiante {
 
 public class Main {
     public static void main(String[] args) {
-        // Objetos creados independientemente
+       
         CursoVirtual c1 = new CursoVirtual("Inteligencia Artificial");
         CursoVirtual c2 = new CursoVirtual("Diseño de Software");
         Estudiante est = new Estudiante("Andres");
 
-        // Acción de matricular
+       
         est.matricular(c1);
         est.matricular(c2);
 
-        // Simular abandono (Demuestra Asociación)
+       
         est.abandonar(c1);
 
-        // Verificación
+       
         System.out.println("Cursos activos para " + est.getNombre() + ":");
         for (CursoVirtual c : est.getMatriculados()) {
             System.out.println(" - " + c.getTitulo());
