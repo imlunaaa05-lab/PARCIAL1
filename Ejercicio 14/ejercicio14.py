5d2d0835363a7ee27dfecb3600d0b5730165bfa9
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-13 10:57:36 -0500

    Update ejercicio14.py

--- Ejercicio 14/ejercicio14.py
+++ Ejercicio 14/ejercicio14.py
@@ -8,23 +8,23 @@ class Escuela:
         self.nombre = nombre
         self.salones = []
 
-    # Agregación: El objeto salón se pasa como parámetro
+    
     def registrar_salon(self, salon):
         self.salones.append(salon)
 
-# --- PRUEBA DE FUNCIONAMIENTO ---
+
 if __name__ == "__main__":
-    # Salones independientes
+   
     lab1 = Salon("Laboratorio 1", 20)
     aula_max = Salon("Aula Máxima", 100)
 
     mi_institucion = Escuela("Instituto Técnico")
     
-    # Agregación
+  
     mi_institucion.registrar_salon(lab1)
     mi_institucion.registrar_salon(aula_max)
 
-    # Verificación
+   
     print(f"Escuela: {mi_institucion.nombre}")
     for s in mi_institucion.salones:
         print(f"  * {s.identificador} - Capacidad: {s.capacidad}")
