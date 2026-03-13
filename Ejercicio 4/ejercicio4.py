ce9702f71f324186a0760ef6e6297b7fb71acad8
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-13 10:48:39 -0500

    Update ejercicio4.py

--- Ejercicio 4/ejercicio4.py
+++ Ejercicio 4/ejercicio4.py
@@ -10,19 +10,19 @@ class Departamento:
     def asignar_empleado(self, empleado):
         self.empleados.append(empleado)
 
-# --- PRUEBA DE FUNCIONAMIENTO ---
+
 if __name__ == "__main__":
-    # 1. Crear objetos por separado (Asociación)
+    
     e1 = Empleado("Laura Casallas")
-    e2 = e1 # Referencia al mismo objeto
+    e2 = e1 
     
     it = Departamento("IT")
     ventas = Departamento("Ventas")
 
-    # 2. Asignar
+ 
     it.asignar_empleado(e1)
     
-    # 3. Corroborar
+ 
     print(f"Departamento: {it.nombre_dep}")
     for emp in it.empleados:
         print(f"  - Trabajador: {emp.nombre}")
