a9e961861fd07a0f00a005cec30d62ee590383c4
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-13 10:56:54 -0500

    Update ejercicio13.py

--- Ejercicio 13/ejercicio13.py
+++ Ejercicio 13/ejercicio13.py
@@ -12,19 +12,19 @@ class Tienda:
     def registrar_producto(self, producto):
         self.productos.append(producto)
 
-# --- PRUEBA DE FUNCIONAMIENTO ---
+
 if __name__ == "__main__":
-    # Productos independientes
+    
     prod1 = Producto("Monitor 4K", 400)
     prod2 = Producto("Teclado Mecánico", 80)
 
     tienda_gamer = Tienda("GamerZone")
     
-    # Agregación
+   
     tienda_gamer.registrar_producto(prod1)
     tienda_gamer.registrar_producto(prod2)
 
-    # Verificación
+   
     print(f"Tienda: {tienda_gamer.nombre}")
     for p in tienda_gamer.productos:
         print(f" - {p.nombre}: ${p.precio}")
