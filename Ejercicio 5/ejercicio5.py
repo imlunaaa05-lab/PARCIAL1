6951c7292dd3e55fa2cf9dcee69bef0f93c09450
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-13 10:50:34 -0500

    Update ejercicio5.py

--- Ejercicio 5/ejercicio5.py
+++ Ejercicio 5/ejercicio5.py
@@ -14,23 +14,22 @@ class Profesor:
     def dictar_asignatura(self, asignatura):
         self.asignaturas.append(asignatura)
 
-# --- PRUEBA DE FUNCIONAMIENTO ---
 if __name__ == "__main__":
-    # 1. Instancias independientes
+   
     p1 = Profesor("Carlos Rodriguez")
-    p2 = Profesor("Martha Gomez")
+    p2 = Profesor("Marta Gomez")
 
     a1 = Asignatura("Inteligencia Artificial")
     a2 = Asignatura("Bases de Datos")
 
-    # 2. Crear vínculos de Asociación
+    
     p1.dictar_asignatura(a1)
     a1.agregar_profesor(p1)
 
     p2.dictar_asignatura(a1)
     a1.agregar_profesor(p2)
 
-    # 3. Corroborar salida
+   
     print(f"La asignatura '{a1.nombre}' tiene los siguientes profesores:")
     for prof in a1.profesores:
         print(f"  - {prof.nombre}")
