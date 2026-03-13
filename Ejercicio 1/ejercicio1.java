e57a80cd370c82b1daee3ae8240b80e7b943be0a
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-13 10:41:17 -0500

    Update ejercicio1.java

--- Ejercicio 1/ejercicio1.java
+++ Ejercicio 1/ejercicio1.java
@@ -30,16 +30,15 @@ class Estudiante {
 
 public class Main {
     public static void main(String[] args) {
-        // Crear objetos independientes
+        
         Curso curso1 = new Curso("Programación Orientada a Objetos");
         Estudiante est1 = new Estudiante("Juan Perez");
         Estudiante est2 = new Estudiante("Maria Lopez");
 
-        // Crear la relación (Asociación)
+    
         curso1.inscribirEstudiante(est1);
         curso1.inscribirEstudiante(est2);
 
-        // Corroborar funcionamiento
         System.out.println("Curso: " + curso1.getNombre());
         for (Estudiante e : curso1.getEstudiantes()) {
             System.out.println(" - Estudiante inscrito: " + e.getNombre());
