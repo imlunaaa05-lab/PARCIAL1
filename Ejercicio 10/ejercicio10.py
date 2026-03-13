f94cd8ea08c81471a645e988ea1194ad87120e41
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-13 10:55:14 -0500

    Update ejercicio10.py

--- Ejercicio 10/ejercicio10.py
+++ Ejercicio 10/ejercicio10.py
@@ -11,11 +11,11 @@ class EmpresaTransporte:
         self.nombre = nombre
         self.flota = []
 
-    # Agregación: El objeto vehiculo ya viene creado
+    
     def registrar_vehiculo(self, vehiculo):
         self.flota.append(vehiculo)
 
-# --- PRUEBA ---
+
 if __name__ == "__main__":
     # Vehículos independientes
     auto1 = Vehiculo("MNO-789", "Van Toyota")
@@ -23,11 +23,11 @@ if __name__ == "__main__":
 
     empresa = EmpresaTransporte("Carga Rápida")
     
-    # Agregación
+
     empresa.registrar_vehiculo(auto1)
     empresa.registrar_vehiculo(auto2)
 
-    # Verificación
+  
     print(f"Empresa: {empresa.nombre}")
     for v in empresa.flota:
         print(f"  * {v}")
